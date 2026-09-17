0083bda6712d9bb102f3805de2105c07905289ee
George Lakoumentas <glakoumentas@my.bcit.ca>
2026-09-17 16:06:45 -0700

    Finished the Week 2 in class Assignment, and made the necessary changes

---
+++ Week 2/Week2_inclass.py
@@ -0,0 +1,14 @@
+x = 16
+y = "George"
+z = 43.5
+
+x = "16"
+print(bool("George"))
+z = 69.69
+print((x))
+print(type(y))
+first_string = "CIT"
+second_string  = "1515"
+print("A" + first_string + second_string)
+assignment_number = 2
+print(f"Assignment {assignment_number} created")
